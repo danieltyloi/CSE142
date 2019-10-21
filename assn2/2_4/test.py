c5498fa573d68162539270a5cc3752cb6fe064c1
Claudio Sangeroki <csangeroki@gmail.com>
2019-10-20 17:03:33 -0700

    Added Euclidean Distance function and improved data handling for CSV reader

--- assn2/2_4/test.py
+++ assn2/2_4/test.py
@@ -1,12 +1,34 @@
-
 import csv
+import math
 
 with open("knn_train.csv","r") as f:
-  reader = csv.reader(f)
-  features = next(reader)
-  print(features)
-  for row in reader:
-    print(row)
+    reader = csv.reader(f)
+  
+    features = next(reader)
+    # print(features)
+  
+    DataList = []
+    for row in reader:
+        DataList += [row]
+        # print(row)
 
+def euclideanDistance(length1, length2):
+    
+    if len(length1) != len(length2):
+        print("Length of data is not equal")
+        return 0
 
+    distance = 0
+    for i in range(len(length1)-1):
+        distance += pow((int(length1[i+1]) - int(length2[i+1])), 2)
 
+    return math.sqrt(distance)
+
+#testData1 = DataList[1].copy()
+#testData2 = DataList[2].copy()
+
+#print(testData1)
+#print(testData2)
+
+#x = euclideanDistance(testData1, testData2)
+#print(x)
