770cabe9cfb3ad3b9906a353838c6c3ade6a7c11
Claudio Sangeroki <csangeroki@gmail.com>
2019-10-20 19:47:19 -0700

    Added manhattan distance and Infinity Norm distance functions

--- assn2/2_4/test.py
+++ assn2/2_4/test.py
@@ -1,6 +1,8 @@
 import csv
 import math
 from operator import itemgetter
+import random
+
 '''
 with open("knn_train.csv","r") as f:
     reader = csv.reader(f)
@@ -14,16 +16,38 @@ with open("knn_train.csv","r") as f:
         print(row)
 '''
 
-def euclideanDistance(length1, length2):
+def manhattanDistance(list1, list2):
 
-    if len(length1) != len(length2):
+    if len(list1) != len(list2):
+        raise Exception('Length of Lists are not equal')
+    
+    distance = 0
+    for i in range(len(list1)-1):
+        distance += abs(int(list1[i+1]) - int(list2[i+1]))
+        
+    return distance
+
+def euclideanDistance(list1, list2):
+
+    if len(list1) != len(list2):
         raise Exception('Length of Lists are not equal')
 
     distance = 0
-    for i in range(len(length1)-1):
-        distance += pow((int(length1[i+1]) - int(length2[i+1])), 2)
+    for i in range(len(list1)-1):
+        distance += pow((int(list1[i+1]) - int(list2[i+1])), 2)
 
     return math.sqrt(distance)
+
+def maxNormDistance(list1, list2):
+    
+    if len(list1) != len(list2):
+        raise Exception('Length of Lists are not equal')
+
+    distance = []
+    for i in range(len(list1)-1):
+        distance.append(abs(int(list1[i+1]) - int(list2[i+1])))
+
+    return max(distance)
 
 #testData1 = DataList[1].copy()
 #testData2 = DataList[2].copy()
@@ -35,7 +59,7 @@ def euclideanDistance(length1, length2):
 #x = euclideanDistance(testData1, testData2)
 #print(x)
 
-def kNearestNeighbors(trainingSet, testValue, k):
+def kNearestNeighbors(trainingSet, testInstance, k):
     '''
     with open("knn_train.csv","r") as f:
         reader = csv.reader(f)
@@ -49,11 +73,12 @@ def kNearestNeighbors(trainingSet, testValue, k):
         
     #Finds distances of testValue to TrainingSet
     for i in trainingSet:
-        dist = euclideanDistance(testValue, i)
+        dist = euclideanDistance(testInstance, i)
         #print(dist)
         distanceList.append((i[0], dist))
-        #print(distanceList)
-    #print(distanceList) 
+    
+    #print(distanceList)
+
     #Sort for nearest distances to get neighbors
     x = sorted(distanceList, key=itemgetter(1))
         
@@ -70,8 +95,10 @@ def prediction(neighbors):
     for i in neighbors:
         counter += int(i[0])
    
-    print(counter)
-    if counter > 0:
+    #print(counter)
+    if counter == 0:
+        return random.choice((-1, 1))
+    elif counter > 0:
         return 1
     else:
         return -1
@@ -87,13 +114,29 @@ def main():
 
         for row in reader:
             DataList += [row]
+        
+        #testData1 = DataList[1].copy()
+        #testData2 = DataList[2].copy()
+        #testData1 .remove('4000')
+
+        #print(testData1)
+        #print(testData2)
+
+        #euclidianTest = euclideanDistance(testData1, testData2)
+        #print(euclidianTest)
+        
+        #manhattanTest = manhattanDistance(testData1, testData2)
+        #print(manhattanTest)
+
+        #maxNormTest = maxNormDistance(testData1, testData2)
+        #print(maxNormTest)
 
         testInstance = ['1','2','9','2250','22']
-        x = kNearestNeighbors(DataList, testInstance, 150)
-        print(x)
+        x = kNearestNeighbors(DataList, testInstance, 4)
+        #print(x)
 
         result = prediction(x)
-        print(result)
-
+        #print(result)
+    
 if __name__ == "__main__":
     main()
