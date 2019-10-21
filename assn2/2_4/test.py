c81ab5367ad0f2b0dca52d3b4220727f9d09d596
Claudio Sangeroki <csangeroki@gmail.com>
2019-10-20 19:10:00 -0700

    Finished KNN Algorithm

--- assn2/2_4/test.py
+++ assn2/2_4/test.py
@@ -1,6 +1,7 @@
 import csv
 import math
-
+from operator import itemgetter
+'''
 with open("knn_train.csv","r") as f:
     reader = csv.reader(f)
   
@@ -10,7 +11,8 @@ with open("knn_train.csv","r") as f:
     DataList = []
     for row in reader:
         DataList += [row]
-        # print(row)
+        print(row)
+'''
 
 def euclideanDistance(length1, length2):
 
@@ -32,3 +34,66 @@ def euclideanDistance(length1, length2):
 
 #x = euclideanDistance(testData1, testData2)
 #print(x)
+
+def kNearestNeighbors(trainingSet, testValue, k):
+    '''
+    with open("knn_train.csv","r") as f:
+        reader = csv.reader(f)
+        features = next(reader)
+        # print(features)
+  
+        DataList = []
+    '''
+
+    distanceList = []
+        
+    #Finds distances of testValue to TrainingSet
+    for i in trainingSet:
+        dist = euclideanDistance(testValue, i)
+        #print(dist)
+        distanceList.append((i[0], dist))
+        #print(distanceList)
+    #print(distanceList) 
+    #Sort for nearest distances to get neighbors
+    x = sorted(distanceList, key=itemgetter(1))
+        
+    neighbors = []
+        
+    for i in range(k):
+        neighbors.append(x[i])
+
+    return neighbors
+
+def prediction(neighbors):
+    counter = 0
+
+    for i in neighbors:
+        counter += int(i[0])
+   
+    print(counter)
+    if counter > 0:
+        return 1
+    else:
+        return -1
+    
+def main():
+
+    with open("knn_train.csv","r") as f:
+        reader = csv.reader(f)
+        features = next(reader)
+        # print(features)
+  
+        DataList = []
+
+        for row in reader:
+            DataList += [row]
+
+        testInstance = ['1','2','9','2250','22']
+        x = kNearestNeighbors(DataList, testInstance, 150)
+        print(x)
+
+        result = prediction(x)
+        print(result)
+
+if __name__ == "__main__":
+    main()
